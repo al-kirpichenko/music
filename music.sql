cea2c17927b82a7b5a488245596db7fe7905ba06
al-kirpichenko <fuluganchik@yandex.ru>
2023-03-08 01:23:07 +0700

    Обновление файла БД

--- music.sql
+++ music.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Мар 05 2023 г., 19:37
+-- Время создания: Мар 07 2023 г., 21:22
 -- Версия сервера: 10.4.12-MariaDB
 -- Версия PHP: 8.0.1
 
@@ -24,17 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `categories`
---
-
-CREATE TABLE `categories` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Структура таблицы `products`
 --
 
@@ -47,73 +36,52 @@ CREATE TABLE `products` (
   `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
 --
--- Структура таблицы `users`
+-- Дамп данных таблицы `products`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `surname` varchar(255) NOT NULL,
-  `patronymic` varchar(255) DEFAULT NULL,
-  `login` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `user_group` int(1) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `users`
---
-
-INSERT INTO `users` (`id`, `name`, `surname`, `patronymic`, `login`, `email`, `password`, `user_group`) VALUES
-(1, 'Александр', 'Кирпиченко', '', 'admin', 'admin@localhost.com', '$2y$10$vWkeCaqRmIlv1kg4W2oIO.fZuloKCQvrQzO7ndDwc6E3LBeAhziSy', 2);
+INSERT INTO `products` (`id`, `name`, `cost`, `category`, `image`, `description`) VALUES
+(4, 'Гитара', 12000, 1, '20230307/1678211717_6c5cc6eb365c71f897e7.jpeg', '1111'),
+(5, 'Гитара', 9000, 1, '20230307/1678211776_306b8223e8262c77e447.jpeg', '66665'),
+(6, 'Гитара', 6000, 1, '20230307/1678211810_5824934798dff3e50a92.jpeg', '14444'),
+(7, 'Скртипка', 17000, 1, '20230307/1678212012_d670cbfdc63cbe07d7e3.jpeg', '7778'),
+(8, 'Скрипка', 14000, 1, '20230307/1678212295_306a4c62c3e4a2a0a2bb.jpeg', '7824424'),
+(9, 'Синтезатор 1', 7800, 2, '20230307/1678212378_e2f3dd0ea87809548acb.jpeg', '646465546'),
+(10, 'Синтезатор 2', 14000, 2, '20230307/1678212486_bdbbc4e7186d1405ce3d.png', 'фапфаифваа'),
+(11, 'Виолончель 1', 23111, 1, '20230307/1678212716_db63b415e80dec46f738.jpeg', '343223'),
+(12, 'Синтезатор 3', 45550, 1, '20230307//1678212807_c11c2ca6af37485da52a.jpeg', '54245234,3'),
+(13, 'Гитара', 51220, 1, '20230307/1678212881_aa9622e8ff36701af734.jpeg', '6536875637');
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
--- Индексы таблицы `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Индексы таблицы `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category` (`category`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
--- AUTO_INCREMENT для таблицы `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT для таблицы `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT для таблицы `users`
+-- Ограничения внешнего ключа сохраненных таблиц
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ограничения внешнего ключа таблицы `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
